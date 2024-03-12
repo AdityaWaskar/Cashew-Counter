ac8321acaba67171338494671a0531a8c3fe8b55
Aditya Waskar <adityawaskar05@gmail.com>
2024-03-12 13:39:38 +0530

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 import { store } from "./store/store.js";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
+    <Analytics />
   </Provider>
 );
